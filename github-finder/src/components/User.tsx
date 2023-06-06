76acf98ca35e41067b1afab76594420dbf12f140
Guilherme Henrique Violin <guilherme.violin@compasso.com.br>
2023-06-06 12:37:26 -0300

    Estilização concluída

--- github-finder/src/components/User.tsx
+++ github-finder/src/components/User.tsx
@@ -4,29 +4,33 @@ import { MdLocationPin } from 'react-icons/md';
 
 import { Link } from 'react-router-dom';
 
+import styles from './User.module.css';
+
 const User = ({avatar_url, username, location, followers, following}: UserProps) => {
   return (
-    <section>
+    <section className={ styles.user }>
       <img src={ avatar_url } alt={ username } />
 
       <h2>{ username }</h2>
 
       {location && (
-        <p>
+        <p className={ styles.location }>
           <MdLocationPin />
 
           <span>{ location }</span>
         </p>
       )}
 
-      <div>
-        <p>{ followers }</p>
-        <p>Seguidores</p>
-      </div>
+      <div className={ styles.stats }>
+        <div>
+          <p className={ styles.number }>{ followers }</p>
+          <p>Seguidores</p>
+        </div>
 
-      <div>
-        <p>Seguindo</p>
-        <p>{ following }</p>
+        <div>
+          <p className={ styles.number }>{ following }</p>
+          <p>Seguindo</p>
+        </div>
       </div>
 
       <Link to={`/repos/${username}`}>Ver melhores projetos</Link>
