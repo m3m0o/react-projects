4a36c4ed45b262e61023f36b48fad79f2bf08e09
Guilherme Henrique Violin <guilherme.violin@compasso.com.br>
2023-06-06 12:26:53 -0300

    Lógica para erro de usuário não encontrado

---
+++ github-finder/src/components/Error.tsx
@@ -0,0 +1,9 @@
+const Error = () => {
+  return (
+    <>
+      <p>Usuário não encontrado!</p>
+    </>
+  )
+}
+
+export default Error;
--- github-finder/src/routes/Home.tsx
+++ github-finder/src/routes/Home.tsx
@@ -4,12 +4,23 @@ import { UserProps } from '../types/user';
 
 import Search from '../components/Search';
 import User from '../components/User';
+import Error from '../components/Error';
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
 
   const loadUser = async(username: string) => {
+    setError(false);
+    setUser(null);
+    
     const response = await fetch(`https://api.github.com/users/${username}`);
+
+    if (response.status === 404) {
+      setError(true);
+
+      return
+    }
 
     const data = await response.json();
 
@@ -31,6 +42,7 @@ const Home = () => {
       <Search loadUser={ loadUser } />
 
       { user && <User {...user} /> }
+      { error && <Error /> }
     </section>
   )
 }
