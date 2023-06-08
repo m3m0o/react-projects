8bf56071a1a01399b9c0c8a37d36ef29fdaa4e4f
m3m0o <guilherme.violin@hotmail.com>
2023-06-08 12:59:04 -0300

    :sparkles: feat: integration with PostgreSQL Database (TypeORM)

--- vendas-online/src/app.module.ts
+++ vendas-online/src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local', '.env.development'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
