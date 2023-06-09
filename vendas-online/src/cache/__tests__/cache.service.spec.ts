846e46eb22e44e990cb817d29c568977577a7dce
m3m0o <guilherme.violin@hotmail.com>
2023-06-09 17:25:34 -0300

    :white_check_mark: test: created tests for CacheService

---
+++ vendas-online/src/cache/__tests__/cache.service.spec.ts
@@ -0,0 +1,46 @@
+import { TestingModule, Test } from '@nestjs/testing';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+
+import { CacheService } from '../cache.service';
+
+import { userEntityMock } from '../../user/__mocks__/user.mock';
+
+describe('CacheService', () => {
+  let service: CacheService;
+  let cacheManager: Cache;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CacheService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: { get: () => userEntityMock, set: () => jest.fn() },
+        },
+      ],
+    }).compile();
+
+    service = module.get<CacheService>(CacheService);
+    cacheManager = module.get(CACHE_MANAGER);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return data in cache', async () => {
+    const user = await service.getCache('key', () => null);
+
+    expect(user).toEqual(userEntityMock);
+  });
+
+  it('should return data in function', async () => {
+    const result = { test: 'test' };
+
+    jest.spyOn(cacheManager, 'get').mockResolvedValueOnce(undefined);
+    const user = await service.getCache('key', () => Promise.resolve(result));
+
+    expect(user).toEqual(result);
+  });
+});
