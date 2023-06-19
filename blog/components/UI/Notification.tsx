1ca140e32dcdceaab92209cc1a0e1d85ad8fae58
m3m0o <guilherme.violin@hotmail.com>
2023-06-19 16:20:11 -0300

    :sparkles: feat: Notification component turned into an React portal

--- blog/components/UI/Notification.tsx
+++ blog/components/UI/Notification.tsx
@@ -23,11 +23,12 @@ function Notification(props: NotificationProps) {
 
   const cssClasses = `${styles.notification} ${statusClasses}`;
 
-  return (
+  return ReactDOM.createPortal(
     <div className={cssClasses}>
       <h2>{title}</h2>
       <p>{message}</p>
-    </div>
+    </div>,
+    document.getElementById('notifications')!
   );
 }
 
