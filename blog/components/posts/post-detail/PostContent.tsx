367ac2fea4f38f4222b3bea47f01af6cce90f4bf
m3m0o <guilherme.violin@hotmail.com>
2023-06-19 13:28:40 -0300

    :lipstick: feat: added better estilization for posts images and code blocks

--- blog/components/posts/post-detail/PostContent.tsx
+++ blog/components/posts/post-detail/PostContent.tsx
@@ -1,4 +1,9 @@
+import Image from 'next/image';
+
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import { Post } from '@/types/post';
 
@@ -15,11 +20,56 @@ const PostContent = (props: PostContentProps) => {
 
   const imagePath = `/img/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={styles.image}>
+            <Image
+              src={image.properties.src}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
+    },
+  };
+
   return (
     <article className={styles.content}>
       <PostHeader title={post.title} image={imagePath} />
 
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
